12046ea08baf90e1e9c4a0a9e005a59ada3610e5
charliekaks <kaksunchained@gmail.com>
2017-06-20 07:53:58 +0300

    styling the maps page

--- js/scripts.js
+++ js/scripts.js
@@ -15,12 +15,14 @@ function initMap() {
         ['NgongLane Kibanda, NRB',-1.3004563, 36.7858376],
         ['Prestige Kibanda, NRB', -1.3004563, 36.7841961],
         ['Adams Kibanda, NRB', -1.3005692, 36.7786378],
-        ['Korosho Kibanda, NRB', -1.3005323, 36.7823955],
-        ['CIC bus-station Kibanda, NRB', -1.3005323, 36.7823955],
-        ['Lenana Kibanda, NRB', -1.3005323, 36.7823955],
-        ['Ole Dumu Kibanda, NRB', -1.3005323, 36.7823955],
-        ['Ngong road kibanda, NRB', -1.3005323, 36.7823955],
-        ['Muringa Kibanda, NRB', -1.3005323, 36.7823955]
+        ['Korosho Kibanda, NRB', -1.2926831, 36.7684812],
+        ['CIC bus-station Kibanda, NRB',-1.300835 ,36.776335 ],
+        ['Lenana Kibanda, NRB',-1.288795 ,36.792146 ],
+        ['Ole Dumu Kibanda, NRB', -1.2927807, 36.7570858],
+        ['Ngong road kibanda, NRB',-1.3006198 , 36.7773287],
+        ['Muringa Kibanda, NRB',-1.296242 ,36.7724823],
+        ['kibra Kibanda, NRB',-1.306643 ,36.782662],
+        ['Muchai Kibanda, NRB',-1.301556 ,36.7978416]
 
     ];
 
@@ -60,11 +62,20 @@ function initMap() {
         ['<div class="info_content">' +
         '<h3>Muringa Kibanda</h3>' +
         '<p>The kibanda is well known for its delicious chapati and the sweet pilau.</p>' +
+        '</div>'],
+        ['<div class="info_content">' +
+        '<h3>Kibra Kibanda</h3>' +
+        '<p>The kibanda is well known for its delicious chapati and the sweet pilau.</p>' +
+        '</div>'],
+        ['<div class="info_content">' +
+        '<h3>Muchai Kibanda</h3>' +
+        '<p>The kibanda is well known for its delicious chapati and the sweet pilau.</p>' +
         '</div>']
     ];
 
     // Add multiple markers to map
     var infoWindow = new google.maps.InfoWindow(), marker, i;
+    //icon style
 
     // Place each marker on the map
     for( i = 0; i < markers.length; i++ ) {
@@ -73,6 +84,7 @@ function initMap() {
         marker = new google.maps.Marker({
             position: position,
             map: map,
+            //icon: 'https://cdn4.iconfinder.com/data/icons/home3/102/Untitled-12-512.png' ,
             title: markers[i][0]
         });
 

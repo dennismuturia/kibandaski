3f12f8620e7a287019b3ca3e04281286563cb6f0
carolynemilgo <carolynemilgo@gmail.com>
2017-06-21 12:54:27 +0300

    added raffle game and updated gallery

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
+var myNumbers=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+var randomArray=[4,8,10,20,25];
+$("#random").click (function(){
+  var number=myNumbers.length;
+  var selected=Math.floor(Math.random()*number);
+  $("#display").text(selected);
+  for(var j=0; j<randomArray.length;j++){
+    if(selected===randomArray[j]){
+      $("#display").text("You have won! Grab yourself free lunch and a 500ml bottle of hot mbuzi bone soup at one of our listed kibandaskis")
+    }
+  }
+  //console.log(selectedt;
+});
 
+new WOW().init();
